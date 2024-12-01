4234701a7102995d6d88f0c6e014c5b5479cf6d2
swetansumohanty <swetansumohanty@gmail.com>
2024-12-01 14:55:42 +0530

    added singleton pattern

---
+++ 05singleton.py
@@ -0,0 +1,43 @@
+"""
+singleton pattern
+=================
+This pattern is useful when you want to create only one instance of the class.
+"""
+
+
+class Metaclass(type):
+    """This is metaclass"""
+
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        # don't create,if the instance is already created
+        if cls not in cls._instance:
+            cls._instance[cls] = super(Metaclass, cls).__call__(*args, **kwargs)
+
+            return cls._instance[cls]
+
+
+class Singleton(metaclass=Metaclass):
+    """This is singleton class"""
+
+    def __init__(self):
+        pass
+
+    def get_item():
+        pass
+
+    pass
+
+
+obj: Singleton = Singleton()
+print(f"i am obj at {obj}")
+
+obj2: Singleton = Singleton()  # o/p: None
+print(f"i am obj at {obj2}")
+
+
+#  when to use singleton design pattern
+# 1 when connecting to third party serive you want only one connection to be opened
+# 2 can be used for logging etc
